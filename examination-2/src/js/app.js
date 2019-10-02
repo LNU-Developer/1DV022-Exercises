18f273e8d85156f228217ed8d1c17b887dc500bf
Rickard Marjanovic <rickard.marjanovic@gmail.com>
2019-10-02 12:58:04 +0200

    Adding references to buttons and creating click eventlisteners

--- examination-2/src/js/app.js
+++ examination-2/src/js/app.js
@@ -1,16 +1,30 @@
 var nextURL
 var api = new window.XMLHttpRequest()
+var startGameBtn
+var answerBtn
 
 function testResponse () {
   console.log(JSON.parse(this.response))
 }
 
 function init () {
+  startGameBtn = document.getElementById('startGameBtn')
+  answerBtn = document.getElementById('answerBtn')
   nextURL = 'http://vhost3.lnu.se:20080/question/1'
+  startGameBtn.addEventListener('click', startGame)
+  answerBtn.addEventListener('click', submitAnswer)
+  console.log('Window loaded')
+}
+window.addEventListener('load', init)
+
+function startGame () {
   api.open('GET', nextURL, true)
   api.setRequestHeader('Content-Type', 'application/json')
   api.send()
   api.addEventListener('load', testResponse)
-  console.log('Window loaded')
+  console.log('Started game')
 }
-window.addEventListener('load', init)
+
+function submitAnswer () {
+  console.log('Answer provided')
+}
