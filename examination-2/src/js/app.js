8180f79e0932b9e5da9a728a2c544ce71f11e297
Rickard Marjanovic <rickard.marjanovic@gmail.com>
2019-10-03 20:27:01 +0200

    Adding eventlistener for xhr load

--- examination-2/src/js/app.js
+++ examination-2/src/js/app.js
@@ -1,9 +1,14 @@
 var nextURL
-var api = new window.XMLHttpRequest()
 var startGameBtn
 var answerBtn
-
+var questionId
+var questionMessage
+// var questionArea
+var apiResponse
 function init () {
+  // questionArea = document.getElementById('questionArea')
+  questionId = document.getElementById('questionId')
+  questionMessage = document.getElementById('questionMessage')
   startGameBtn = document.getElementById('startGameBtn')
   answerBtn = document.getElementById('answerBtn')
   nextURL = 'http://vhost3.lnu.se:20080/question/1'
@@ -14,24 +19,33 @@ function init () {
 window.addEventListener('load', init)
 
 function useApi (type, answer) {
+  var api = new window.XMLHttpRequest()
+
+  api.addEventListener('load', apiData)
   api.open(type, nextURL, true)
   api.setRequestHeader('Content-Type', 'application/json')
   api.responseType = 'json'
   api.send(JSON.stringify(answer))
-  api.onreadystatechange = function () {
-    if (api.readyState === 4 && api.status === 200) {
-      nextURL = this.response.nextURL
-      console.log(this.response)
-      console.log(nextURL)
-    }
+}
+
+function apiData () {
+  apiResponse = this.response
+  nextURL = this.response.nextURL
+  console.log(this.response)
+  console.log(nextURL)
+
+  if (apiResponse.message === 'You got your question! Now send me the answer via HTTP POST to the nextURL in JSON-format' || 'You got your question! Now send me which alternative that is right (the key) as the answer via HTTP POST to the nextURL in JSON-format') {
+    questionMessage.innerHTML = apiResponse.question
+    questionId.innerHTML = apiResponse.id
   }
 }
 
 function startGame () {
   useApi('GET')
-  console.log('Started game')
+  startGameBtn.disabled = true
 }
 
 function submitAnswer () {
   useApi('POST', { answer: 2 })
+  startGameBtn.disabled = false
 }
