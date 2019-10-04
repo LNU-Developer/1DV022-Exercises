6b923decb6461e5825852af1a4c74da427b1245c
Rickard Marjanovic <rickard.marjanovic@gmail.com>
2019-10-04 15:39:37 +0200

    Adding promise wrapper and async functions

--- examination-2/src/js/app.js
+++ examination-2/src/js/app.js
@@ -1,14 +1,17 @@
 var nextURL
 var startGameBtn
 var answerBtn
-var questionId
-var questionMessage
+// var questionId
+// var questionMessage
+// var answerType
+// var submittedAnswer
+var response
 // var questionArea
-var apiResponse
 function init () {
   // questionArea = document.getElementById('questionArea')
-  questionId = document.getElementById('questionId')
-  questionMessage = document.getElementById('questionMessage')
+  // answerType = document.getElementById('answerType')
+  // questionId = document.getElementById('questionId')
+  // questionMessage = document.getElementById('questionMessage')
   startGameBtn = document.getElementById('startGameBtn')
   answerBtn = document.getElementById('answerBtn')
   nextURL = 'http://vhost3.lnu.se:20080/question/1'
@@ -19,33 +22,40 @@ function init () {
 window.addEventListener('load', init)
 
 function useApi (type, answer) {
-  var api = new window.XMLHttpRequest()
-
-  api.addEventListener('load', apiData)
-  api.open(type, nextURL, true)
-  api.setRequestHeader('Content-Type', 'application/json')
-  api.responseType = 'json'
-  api.send(JSON.stringify(answer))
+  return new Promise(function (resolve) {
+    var api = new window.XMLHttpRequest()
+    api.open(type, nextURL, true)
+    api.setRequestHeader('Content-Type', 'application/json')
+    api.responseType = 'json'
+    api.onload = function () {
+      nextURL = api.response.nextURL
+      return resolve(api.response)
+    }
+    api.send(JSON.stringify(answer))
+  })
 }
 
-function apiData () {
-  apiResponse = this.response
-  nextURL = this.response.nextURL
-  console.log(this.response)
-  console.log(nextURL)
+// function apiData () {
+// svar = this.response
+// nextURL = this.response.nextURL
+// console.log(this.response)
+// console.log(nextURL)
 
-  if (apiResponse.message === 'You got your question! Now send me the answer via HTTP POST to the nextURL in JSON-format' || 'You got your question! Now send me which alternative that is right (the key) as the answer via HTTP POST to the nextURL in JSON-format') {
-    questionMessage.innerHTML = apiResponse.question
-    questionId.innerHTML = apiResponse.id
-  }
+// if (this.response.message === 'You got your question! Now send me the answer via HTTP POST to the nextURL in JSON-format') {
+// questionMessage.innerHTML = this.response.question
+// questionId.innerHTML = this.response.id
+// answerType.innerHTML = '<input type="text" name="answer" id="userAnswer" size="5">'
+// submittedAnswer = document.getElementById('userAnswer').value
+// console.log(submittedAnswer)
+// } // else if (|| 'You got your question! Now send me which alternative that is right (the key) as the answer via HTTP POST to the nextURL in JSON-format'))
+// }
+
+async function startGame () {
+  response = await useApi('GET')
+  console.log(response)
 }
 
-function startGame () {
-  useApi('GET')
-  startGameBtn.disabled = true
-}
-
-function submitAnswer () {
-  useApi('POST', { answer: 2 })
-  startGameBtn.disabled = false
+async function submitAnswer () {
+  response = await useApi('POST', { answer: 2 })
+  console.log(response)
 }
