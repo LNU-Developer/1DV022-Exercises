f01503c403bbba30c94397820f7d2536d9a62d62
Rickard Marjanovic <rickard.marjanovic@gmail.com>
2019-10-19 09:29:55 +0200

    Adding possibility to send messages

--- examination-3/src/js/websocket.js
+++ examination-3/src/js/websocket.js
@@ -1,25 +1,34 @@
 var nickname
 var userMessage
+var receivedMessages
 
 function init () {
   nickname = document.getElementById('nickname')
   userMessage = document.getElementById('userMessage')
+  receivedMessages = document.getElementById('receivedMessages')
 }
 window.addEventListener('load', init)
 
 const ws = new window.WebSocket('ws://188.166.67.186:9080')
+
 ws.addEventListener('open', event => {
-  console.log(ws.readyState)
+  receivedMessages.innerHTML += 'Welcome! Please type something to start a conversation. Remember the channels are monitored and abuse will not be allowed' + '<br>'
 })
+
 ws.addEventListener('message', event => {
-  console.log(event.data)
+  const data = JSON.parse(event.data)
+  if (data.username !== 'The Server') {
+    receivedMessages.innerHTML += data.username + '<br>'
+    receivedMessages.innerHTML += data.data + '<br>'
+  }
 })
+
 document.getElementById('sendMessage').addEventListener('click', event => {
   const data = {
     type: 'message',
     data: userMessage.value,
     username: nickname.value,
-    channel: 'my, not so secret, channel',
+    channel: 'channel',
     key: 'asldfkjasdlkfj' }
   ws.send(JSON.stringify(data))
 })
