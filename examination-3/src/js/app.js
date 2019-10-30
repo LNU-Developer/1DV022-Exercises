1ff5384dab184015e7f0b562040aba340f0f3d2b
Rickard Marjanovic <rickard.marjanovic@gmail.com>
2019-10-30 22:50:48 +0100

    Adding import/export, refactoring Chat.js by wrapping in a class as well as enabling several instances to the Chat

--- examination-3/src/js/app.js
+++ examination-3/src/js/app.js
@@ -1,39 +1,34 @@
+import { Chat } from './chat.js'
+let count = 0
 
-var chat = `
-  <div id="nicknameArea">
-  <h2>Enter your nickname</h2>
-  <p><input type="text" name="name" id="nickname" size="20"></p>
-  </div>
-  <div id="messageArea">
-  <h2>Messages</h2>
-  <span id="receivedMessages"></span>
-  </div>
-  <div id="inputArea">
-  <h2>Type your message</h2>
-  <p><textarea rows="1" cols="50" id="userMessage"></textarea></p>
-  </div>
-  <button type="button" id="sendMessage">Send message</button>
-  `
-var home = ``
+// const home = ``
 
-const routes = {
-  '/': home,
-  '/chat': chat
-}
+// const routes = {
+//  '/': home,
+//  '/chat': chat
+// }
 
 const root = document.getElementById('root')
-root.innerHTML = routes[window.location.pathname]
 
 document.getElementById('chat').addEventListener('click', function (pathname) {
   pathname = '/chat' // temp fix
-  window.history.pushState(
-    {},
-    pathname,
-    window.location.origin + pathname
-  )
-  root.innerHTML = routes[pathname]
+
+  if (pathname === '/chat') {
+    window.history.pushState(
+      {},
+      pathname,
+      window.location.origin + pathname
+    )
+
+    var test = new Chat(count)
+    test.startChat()
+    root.insertAdjacentHTML('beforeend', test.chat)
+    test.startEvents()
+    test.sendMessage()
+    count++
+  }
 })
 
-window.onpopstate = () => {
-  root.innerHTML = routes[window.location.pathname]
-}
+// window.onpopstate = () => {
+// root.innerHTML = routes[window.location.pathname]
+// }
--- examination-3/src/js/chat.js
+++ examination-3/src/js/chat.js
@@ -1,25 +1,57 @@
-
-const ws = new window.WebSocket('ws://188.166.67.186:9080')
-
-ws.addEventListener('message', event => {
-  const receivedMessages = document.getElementById('receivedMessages')
-  const data = JSON.parse(event.data)
-  if (data.type === 'message') {
-    receivedMessages.innerHTML += data.username + '<br>'
-    receivedMessages.innerHTML += data.data + '<br><br>'
+class Chat {
+  constructor (count) {
+    this.count = count
+    this.chat = `
+                <div id="nicknameArea">
+                <h2>Enter your nickname</h2>
+                <p><input type="text" name="name" id="nickname${this.count}" size="20"></p>
+                </div>
+                <div id="messageArea">
+                <h2>Messages</h2>
+                <span id="receivedMessages${this.count}"></span>
+                </div>
+                <div id="inputArea">
+                <h2>Type your message</h2>
+                <p><textarea rows="1" cols="50" id="userMessage${this.count}"></textarea></p>
+                </div>
+                <button type="button" id="sendMessage${this.count}">Send message</button>
+                `
   }
-})
 
-document.addEventListener('click', function (event) {
-  if (event.target.id && event.target.id === 'sendMessage') {
-    const data = {
-      type: 'message',
-      data: document.getElementById('userMessage').value,
-      username: document.getElementById('nickname').value,
-      channel: 'channel',
-      key: 'asldfkjasdlkfj'
-    }
-    ws.send(JSON.stringify(data))
-    document.getElementById('userMessage').value = ''
+  startChat () {
+    this.ws = new window.WebSocket('ws://188.166.67.186:9080')
   }
-})
+
+  closeChat () {
+    this.ws.close()
+  }
+
+  startEvents () {
+    this.ws.addEventListener('message', event => {
+      const receivedMessages = document.getElementById(`receivedMessages${this.count}`)
+      const data = JSON.parse(event.data)
+      if (data.type === 'message' || data.type === 'notification') {
+        receivedMessages.innerHTML += data.username + '<br>'
+        receivedMessages.innerHTML += data.data + '<br><br>'
+      }
+    })
+  }
+
+  sendMessage () {
+    const sendMessage = document.getElementById(`sendMessage${this.count}`)
+    sendMessage.addEventListener('click', function clickButton (event) {
+      console.log(this.count)
+      const data = {
+        type: 'message',
+        data: document.getElementById(`userMessage${this.count}`).value,
+        username: document.getElementById(`nickname${this.count}`).value,
+        channel: 'channel',
+        key: 'asldfkjasdlkfj'
+      }
+      this.ws.send(JSON.stringify(data))
+      // document.getElementById(`userMessage${this.count}`).value = ''
+    }.bind(this))
+  }
+}
+
+export { Chat }
