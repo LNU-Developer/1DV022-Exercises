69e4aa0efc2ae5e9dd4ee0f9eb0f31b781e4a89f
Rickard Marjanovic <rickard.marjanovic@gmail.com>
2019-11-28 21:44:56 +0100

    Adding keyboard usage functionality

--- examination-3/src/js/Memory.js
+++ examination-3/src/js/Memory.js
@@ -3,22 +3,22 @@ class Memory {
     this.count = count
     this.memory = `
 <div id="bricks${this.count}" style="width: 250px; display: inline-block;">
-<img src="image/0.png" alt="spelbricka" class="" id="0" width="60px" height="60px" margin="2px">
-<img src="image/0.png" alt="spelbricka" class="" id="1" width="60px" height="60px" margin="2px">
-<img src="image/0.png" alt="spelbricka" class="" id="2" width="60px" height="60px" margin="2px">
-<img src="image/0.png" alt="spelbricka" class="" id="3" width="60px" height="60px" margin="2px">
-<img src="image/0.png" alt="spelbricka" class="" id="4" width="60px" height="60px" margin="2px">
-<img src="image/0.png" alt="spelbricka" class="" id="5" width="60px" height="60px" margin="2px">
-<img src="image/0.png" alt="spelbricka" class="" id="6" width="60px" height="60px" margin="2px">
-<img src="image/0.png" alt="spelbricka" class="" id="7" width="60px" height="60px" margin="2px">
-<img src="image/0.png" alt="spelbricka" class="" id="8" width="60px" height="60px" margin="2px">
-<img src="image/0.png" alt="spelbricka" class="" id="9" width="60px" height="60px" margin="2px">
-<img src="image/0.png" alt="spelbricka" class="" id="10" width="60px" height="60px" margin="2px">
-<img src="image/0.png" alt="spelbricka" class="" id="11" width="60px" height="60px" margin="2px">
-<img src="image/0.png" alt="spelbricka" class="" id="12" width="60px" height="60px" margin="2px">
-<img src="image/0.png" alt="spelbricka" class="" id="13" width="60px" height="60px" margin="2px">
-<img src="image/0.png" alt="spelbricka" class="" id="14" width="60px" height="60px" margin="2px">
-<img src="image/0.png" alt="spelbricka" class="" id="15" width="60px" height="60px" margin="2px">
+<img src="image/0.png" alt="Brick" tabindex="0" id="0" width="60px" height="60px" margin="2px">
+<img src="image/0.png" alt="Brick" tabindex="0" id="1" width="60px" height="60px" margin="2px">
+<img src="image/0.png" alt="Brick" tabindex="0" id="2" width="60px" height="60px" margin="2px">
+<img src="image/0.png" alt="Brick" tabindex="0" id="3" width="60px" height="60px" margin="2px">
+<img src="image/0.png" alt="Brick" tabindex="0" id="4" width="60px" height="60px" margin="2px">
+<img src="image/0.png" alt="Brick" tabindex="0" id="5" width="60px" height="60px" margin="2px">
+<img src="image/0.png" alt="Brick" tabindex="0" id="6" width="60px" height="60px" margin="2px">
+<img src="image/0.png" alt="Brick" tabindex="0" id="7" width="60px" height="60px" margin="2px">
+<img src="image/0.png" alt="Brick" tabindex="0" id="8" width="60px" height="60px" margin="2px">
+<img src="image/0.png" alt="Brick" tabindex="0" id="9" width="60px" height="60px" margin="2px">
+<img src="image/0.png" alt="Brick" tabindex="0" id="10" width="60px" height="60px" margin="2px">
+<img src="image/0.png" alt="Brick" tabindex="0" id="11" width="60px" height="60px" margin="2px">
+<img src="image/0.png" alt="Brick" tabindex="0" id="12" width="60px" height="60px" margin="2px">
+<img src="image/0.png" alt="Brick" tabindex="0" id="13" width="60px" height="60px" margin="2px">
+<img src="image/0.png" alt="Brick" tabindex="0" id="14" width="60px" height="60px" margin="2px">
+<img src="image/0.png" alt="Brick" tabindex="0" id="15" width="60px" height="60px" margin="2px">
 </div> <!-- End bricks -->
 `
     this.frontPic = []
@@ -31,11 +31,32 @@ class Memory {
       this.picsElems[i].src = '/examination-3/src/image/0.png'
     }
     this.ranomdizePics()
-    document.getElementById(`bricks${this.count}`).addEventListener('click', this.clickedImg.bind(this))
+    this.picsElems[0].focus()
+    document.getElementById(`bricks${this.count}`).addEventListener('click', this.selectImg.bind(this))
+    document.getElementById(`bricks${this.count}`).addEventListener('keydown', this.keyUse.bind(this))
   }
 
-  clickedImg (event) {
-    if (!isNaN(event.target.id) && this.turnedPic.length < 2) {
+  keyUse (event) {
+    const id = Number(document.activeElement.id)
+    if (event.keyCode === 13 || event.keyCode === 32) {
+      this.selectImg(event)
+    }
+    if (event.keyCode === 37 && id !== 0) {
+      this.picsElems[id].previousElementSibling.focus()
+    }
+    if (event.keyCode === 39 && id !== this.picsElems.length - 1) {
+      this.picsElems[id].nextElementSibling.focus()
+    }
+    if (event.keyCode === 38 && id > 3) {
+      this.picsElems[id - 4].focus()
+    }
+    if (event.keyCode === 40 && id < this.picsElems.length - 4) {
+      this.picsElems[id + 4].focus()
+    }
+  }
+
+  selectImg (event) {
+    if (!isNaN(event.target.id) && this.turnedPic.length < 2 && event.target.tabIndex === 0) {
       this.picsElems[event.target.id].src = `/examination-3/src/image/${[this.frontPic[event.target.id]]}.png`
       this.turnedPic.push(event.target.id)
       if (this.turnedPic.length === 2) {
@@ -52,8 +73,10 @@ class Memory {
 
     if (first !== second) {
       if (this.frontPic[first] === this.frontPic[second]) {
-        this.picsElems[first].style.visibility = 'hidden'
-        this.picsElems[second].style.visibility = 'hidden'
+        this.picsElems[first].style.opacity = 0
+        this.picsElems[second].style.opacity = 0
+        this.picsElems[first].tabIndex = -1
+        this.picsElems[second].tabIndex = -1
       } else {
         this.picsElems[first].src = '/examination-3/src/image/0.png'
         this.picsElems[second].src = '/examination-3/src/image/0.png'
